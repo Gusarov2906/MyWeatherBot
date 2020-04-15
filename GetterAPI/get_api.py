4696a10a0c13957ca11362bc5f9f5709bce9db5b
Gusarov2906 <gusarov2906@gmail.com>
2020-04-15 18:26:29 +0300

    Script getting current weather from OpenWeatherMap.com

---
+++ GetterAPI/get_api.py
@@ -0,0 +1,17 @@
+import configparser
+import requests
+import sys
+appid = "09f2551c5cf86a1131a05a2bcb397626"
+city_id = "463829"
+try:
+    res = requests.get("http://api.openweathermap.org/data/2.5/weather",
+                 params={'id': city_id, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+    data = res.json()
+    print("data:",data)
+    print("conditions:", data['weather'][0]['description'])
+    print("temp:", data['main']['temp'])
+    print("temp_min:", data['main']['temp_min'])
+    print("temp_max:", data['main']['temp_max'])
+except Exception as e:
+    print("Exception (weather):", e)
+    pass
