fc1996e1612db9db25adc34bf909c090390fdbd6
gusarov2906 <gusarov2906@gmail.com>
2020-05-17 13:18:42 +0300

    Changes in functions to get data from API

--- BOT/GetterAPI/get_api.py
+++ BOT/GetterAPI/get_api.py
@@ -1,20 +1,31 @@
 #>cd E:\PROJECTS\PythonProjects\MyWeatherBot\MyWeatherBot\GetterAPI>
 import json
-from GetterAPI.OpenWeatherAPI import get_data
+from GetterAPI.OpenWeatherAPI import get_weather_data
+from GetterAPI.OpenWeatherAPI import get_forecast_data
 #import OpenWeatherAPI
-def get():
+def get_current_weather():
     city_id = "463829"
     try:
-        weather,forecast = get_data(city_id)
+        weather = get_weather_data(city_id)
     #    weather,forecast = OpenWeatherAPI.get_data(city_id)
         with open(r"E:\PROJECTS\PythonProjects\MyWeatherBot\MyWeatherBot\BOT\data_cur_weather.json",'w',encoding='utf-8') as file:
+    #    with open(r"E:/PROJECTS/PythonProjects/MyWeatherBot/MyWeatherBot/BOT/data_cur_weather.json",'w',encoding='utf-8') as file:
     #        file.write("WEATHER: \n")
     #        for item in weather.items():
     #            file.write("\n" +str(item)
             json.dump(weather,file)
             file.close()
+            print("Upload to file was successful!")
+    except Exception as e:
+        print("Exception: ", e)
+        pass
 
+def get_forecast_weather():
+    city_id = "463829"
+    try:
+        forecast = get_forecast_data(city_id)
         with open(r"E:\PROJECTS\PythonProjects\MyWeatherBot\MyWeatherBot\BOT\data_forecast.json",'w',encoding='utf-8') as file:
+    #    with open(r"E:/PROJECTS/PythonProjects/MyWeatherBot/MyWeatherBot/BOT/data_forecast.json",'w',encoding='utf-8') as file:
     #        file.write("FORECAST: \n")
     #        for time in forecast["list"]:
     #            file.write("\n\n")
@@ -31,4 +42,5 @@ def get():
         pass
 
 if __name__ == '__main__':
-    get()
+    get_current_weather()
+    get_forecast_weather()
