89ef647d3f5a8074a3094516cc20d653d331eff9
gusarov2906 <gusarov2906@gmail.com>
2020-05-20 16:38:59 +0300

    remove old files

--- GetterAPI/OpenWeatherAPI.py
+++
@@ -1,9 +0,0 @@
-import requests
-def get_data(city_id):
-    res=[]
-    appid = "xxx"
-    res.append(requests.get(f"http://api.openweathermap.org/data/2.5/weather?id={city_id}&APPID={appid}&units=metric"))
-    res.append(requests.get(f"http://api.openweathermap.org/data/2.5/forecast?id={city_id}&APPID={appid}&units=metric"))
-    weather = res[0].json()
-    forecast = res[1].json()
-    return weather,forecast
